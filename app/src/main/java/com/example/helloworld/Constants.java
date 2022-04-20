1bde7eece993aafb92995a7f5e696ee73fff8984
arerde <arerde@uw.edu>
2022-04-19 23:20:04 -0700

    Week2

---
+++ app/src/main/java/com/example/helloworld/Constants.java
@@ -0,0 +1,6 @@
+package com.example.helloworld;
+
+public class Constants {
+    public static final String USERNAME_KEY = "username";
+    public static final String SELECTED_DOB = "selectedDob";
+}
